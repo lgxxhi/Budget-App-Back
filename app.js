13097365638caf805f4d9b4c6ff780928acada4a
lgxxhi <luisgsilva95@gmail.com>
2023-07-10 19:27:23 -0400

    More changes

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const cors = require("cors");
 const app = express();
 
 const transactionController = require("./controllers/transactionController");
+const indexController = require("./controllers/indexController");
 
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
 
+app.use("/", indexController);
 app.use("/transactions", transactionController);
 
 app.use("*", (req, res) => {
